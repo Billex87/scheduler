2cd9781aa28893eccd7d5dcf6a82cc06bcf401a9
Billy McKinnon <Billy_McKinnon@hotmail.com>
2021-02-15 19:20:31 -0800

    Day 1 Complete

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,6 +3,7 @@ import React from "react";
 import "components/Application.scss";
 
 export default function Application(props) {
+  const [day, setDay] = useState("Monday");
   return (
     <main className="layout">
       <section className="sidebar">
@@ -12,7 +13,10 @@ export default function Application(props) {
       alt="Interview Scheduler"
     />
     <hr className="sidebar__separator sidebar--centered" />
-    <nav className="sidebar__menu"></nav>
+    <nav className="sidebar__menu"><DayList
+    days={days}
+    day={day}
+    setDay={setDay}/></nav>
     <img
       className="sidebar__lhl sidebar--centered"
       src="images/lhl.png"
@@ -20,7 +24,7 @@ export default function Application(props) {
     />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        
       </section>
     </main>
   );
